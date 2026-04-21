c5ea4010d3c368b05d4cb3243e4728f5e7fe2f8f
lanchenbayumkhaibam7-star <lanchenbayumkhaibam7@gmail.com>
2026-04-21 21:40:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -241,7 +241,7 @@ const STUDENTS = [
     meme: null, reason: null },
   { name: "KSHETRIMAYUM BILLION SINGH",
     roll: "NDUIBC2501", course: "BCA (4 Years)", section: "Section A",
-    meme: "images/yourmeme.jpg", reason:"This meme represents my humor"},
+    meme:pull, reason: pull},
   { name: "YUKHAIBAM LANCHENBA LUWANG",
     roll: "NDUIBC2502", course: "BCA (4 Years)", section: "Section A",
     meme: "images/yourmeme.jpg", reason: "This meme represents my humor" },
